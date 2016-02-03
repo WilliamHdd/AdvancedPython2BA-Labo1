f887ff45f2efeb48e2944b41121236eca7eb315f
WilliamHdd <williamhdd@yahoo.fr>
2016-02-03 15:17:12 +0100

    add unit test for utils module

---
+++ test_utils.py
@@ -0,0 +1,25 @@
+# test_utils.py
+# Author: Sébastien Combéfis
+# Version: February 2, 2016
+
+import unittest
+import utils
+
+class TestUtils(unittest.TestCase):
+    def test_fact(self):
+        if n <0:
+            raise ValueError
+        pass
+    
+    def test_roots(self):
+        # À compléter...
+        pass
+    
+    def test_integrate(self):
+        assert lower > upper, "Problèmes aux bornes!"
+        pass
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestUtils)
+    runner = unittest.TextTestRunner()
+    exit(not runner.run(suite).wasSuccessful())
